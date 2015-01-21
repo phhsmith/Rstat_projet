f5a52c298df21e52d5de8f2266aa2e6dc646bba4
DaphneRR <d.rimskyrobert@gmail.com>
2015-01-21 18:50:16 +0100

    Update code_reponses.R
    importation du fichier dans R

--- code_reponses.R
+++ code_reponses.R
@@ -1,1 +1,12 @@
-#
+library(foreign)
+
+
+# ici il faudra changer ton wd pour le dossier qui contient  "01_weights.sav"
+wd = "C:/Users/SexyManatee/Desktop/PhD/Cogmaster/r-cogstats/projects" 
+
+# lecture du fichier et importation du data.frame
+w <- read.spss(paste(wd,"01-weights.sav", sep = '/'), to.data.frame = TRUE)
+
+#pour vérifier que les noms sont bien là et que tout est en ordre, à comparer avec le fichier de description donné par Lalanne.
+names(w)
+str(w)
